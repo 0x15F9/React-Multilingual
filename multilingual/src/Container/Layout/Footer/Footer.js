94c23f8cca355f445b7f75ea28494da7b7f4f59b
0x15F9 <isfaaq@gmail.com>
2019-06-21 09:21:59 +0400

    Implement Footer

---
+++ multilingual/src/Container/Layout/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer class="page-footer">
+          <div class="container">
+          </div>
+          <div class="footer-copyright">
+            <div class="container center-align">
+            Made by Isfaaq G.
+            </div>
+          </div>
+        </footer>
+    );
+}
+
+export default Footer;
--- multilingual/src/Container/Layout/Layout.js
+++ multilingual/src/Container/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Navbar from './Navbar/Navbar';
+import Footer from './Footer/Footer';
 
 const Layout = (props) => {
     return (
         <div>
             <Navbar />
+            <Footer />
         </div>
     )
 }
