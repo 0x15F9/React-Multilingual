c37f1421047d9fd9ba273f9f7ab6057880ab08c0
0x15F9 <isfaaq@gmail.com>
2019-06-21 09:33:57 +0400

    Implement Homepage

--- multilingual/src/Container/Layout/Layout.js
+++ multilingual/src/Container/Layout/Layout.js
@@ -3,10 +3,13 @@ import React from 'react';
 import Navbar from './Navbar/Navbar';
 import Footer from './Footer/Footer';
 
+import Home from './Pages/Home/Home';
+
 const Layout = (props) => {
     return (
         <div>
             <Navbar />
+                <Home />
             <Footer />
         </div>
     )
---
+++ multilingual/src/Container/Layout/Pages/Home/Home.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            
+            <div class="card-panel center-align">
+                <h1>Homepage</h1>
+                <h2>Work in Progress... </h2>
+                <div class="preloader-wrapper active">
+                <div class="spinner-layer spinner-red-only">
+                    <div class="circle-clipper left">
+                        <div class="circle"></div>
+                    </div><div class="gap-patch">
+                        <div class="circle"></div>
+                    </div><div class="circle-clipper right">
+                        <div class="circle"></div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    )
+}
+
+export default Home;
